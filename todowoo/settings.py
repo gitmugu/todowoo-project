fc33b02e0afda95bfd75b7a672feb7b1c7dc73d8
gitmugu <mugu.862gmail.com>
2020-04-22 18:24:36 +0000

    Removed cache

--- todowoo/settings.py
+++ todowoo/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '^n#rcw=3u&idn28hx_4c&5k8ne&-%#!o=0)xg!4i5#3))+ztbu'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['mugundhan86.pythonanywhere.com']
 
 
 # Application definition
@@ -119,5 +119,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR,'static')
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 
 LOGIN_URL = '/login'
